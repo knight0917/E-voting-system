8ae650fde04e24f8fcc3290a388573b5a3c22014
knightMoon <86282274+knight0917@users.noreply.github.com>
2025-12-26 16:55:27 +0100

    Delete admin directory

--- admin/includes/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
-      <b>Voting System</b>
-    </div>
-    <strong> &copy; 2018 Brought To You By <a href="https://code-projects.org/">Code-Projects</a></strong>
-</footer>
